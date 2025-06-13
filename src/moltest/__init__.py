2f5a8f6cb49c471d3f9c95ccb4e60a6e9e1604fd
Eddie Dunn <gdunn6@oreillyauto.com>
2025-06-13 11:28:09 -0500

    feat: use importlib.metadata to dynamically fetch package version

--- src/moltest/__init__.py
+++ src/moltest/__init__.py
@@ -1,2 +1,7 @@
 # moltest/__init__.py
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # type: ignore
+
+__version__ = version("moltest")
